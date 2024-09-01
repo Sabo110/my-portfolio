59fcc52f3f4371194d49b0f032b3138de151f9e0
unknown <samirphanuel@gmail.com>
2024-09-01 15:29:19 +0100

    change du bg dans proexp

--- app/components/home/ProExp.tsx
+++ app/components/home/ProExp.tsx
@@ -17,7 +17,7 @@ export default function ProExp() {
             <div className='flex flex-col gap-3 mt-3'>
                 {isPending && <><ProExpSkeleton /></>}
                 {data?.map((item) => (
-                    <div className='border rounded shadow-xl p-3 bg-gray-800 text-gray-300' key={item.id}>
+                    <div className='border rounded shadow-xl p-3 bg-gray-900 text-gray-300' key={item.id}>
                         <ul className='flex flex-col gap-1'>
                             <li><span className='capitalize'>Entreprise: </span> <span className='font-light'>{item.ogarnization_name}</span></li>
                             <li><span className='capitalize'>poste: </span> <span>{item.job_title}</span></li>
